fc7fd879cc14966f19566868def7196ff4c05806
Jericho0318 <jiros@DESKTOP-PI4OJSK.localdomain>
2025-10-03 19:26:23 -0700

    Added clothing page

---
+++ src/app/clothing/womens/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../../header/page";
+export default function Womens() {
+  return (
+    <div>
+        <Header />
+        <main>
+            <div id="description" className=" w-2/3 mx-auto">
+              <div id="blurb" className="blurb">
+                <p>Womens clothing section.</p>
+              </div>
+            </div>
+        </main>
+    </div>
+  );
+}
